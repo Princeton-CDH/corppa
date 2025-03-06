a8e7f47a76a345e1eeb92df2751a1290c7e1f2e0
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-03-06 11:37:13 -0500

    Added additional unit test

--- test/test_poetry_detection/test_passim/test_get_passim_results.py
+++ test/test_poetry_detection/test_passim/test_get_passim_results.py
@@ -1,8 +1,12 @@
+import pathlib
 from unittest.mock import call, patch
+
+import pytest
 
 from corppa.poetry_detection.core import LabeledExcerpt
 from corppa.poetry_detection.passim.get_passim_results import (
     build_passim_excerpt,
+    extract_passim_spans,
     get_page_texts,
     get_passim_span,
 )
@@ -84,3 +88,43 @@ def test_get_passim_span():
         "aligned_ref_excerpt": "Aligned text with dashes",
         "aligned_ppa_excerpt": "Aligned text with ------",
     }
+
+
+@patch("corppa.poetry_detection.passim.get_passim_results.get_passim_span")
+@patch("corppa.poetry_detection.passim.get_passim_results.orjsonl")
+def test_extract_passim_spans(mock_orjsonl, mock_get_span, tmp_path):
+    # Build test input directory
+    ## Create top-level passim directory
+    passim_dir = tmp_path / "passim_output"
+    passim_dir.mkdir()
+    ## Create align directory
+    align_dir = passim_dir / "align.json"
+    align_dir.mkdir()
+    ## Create some input files
+    input_a = align_dir / "a.json"
+    input_a.touch()
+    input_b = align_dir / "b.json"
+    input_b.touch()
+    ## An ignored file
+    ignore_c = align_dir / "c.txt"
+    ignore_c.touch()
+
+    # Error on bad input directory
+    error_message = (
+        f"Error: Alignment directory '{align_dir}/align.json' does not exist"
+    )
+    with pytest.raises(ValueError, match=error_message):
+        _ = list(extract_passim_spans(align_dir))
+    mock_orjsonl.assert_not_called()
+    mock_get_span.assert_not_called()
+
+    # Typical case
+    mock_orjsonl.stream.side_effect = [iter(["a1"]), iter(["b1", "b2"])]
+    mock_get_span.side_effect = ["r1", "r2", "r3"]
+
+    results = list(extract_passim_spans(passim_dir))
+    assert results == ["r1", "r2", "r3"]
+    assert mock_orjsonl.stream.call_count == 2
+    mock_orjsonl.stream.assert_has_calls([call(input_a), call(input_b)])
+    assert mock_get_span.call_count == 3
+    mock_get_span.assert_has_calls([call("a1"), call("b1"), call("b2")])
