b7167e616fabb9aacc213ecde8bea528ad71cc46
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-03-04 09:33:45 -0500

    Expand notebook to look at poem ids that are collapsed

--- notebooks/explore-merged-excerpts.py
+++ notebooks/explore-merged-excerpts.py
@@ -22,9 +22,12 @@ def _():
     import polars as pl
 
     from corppa.config import get_config
-    from corppa.poetry_detection.polars_utils import load_excerpts_df
+    from corppa.poetry_detection.polars_utils import (
+        add_ref_poems_meta,
+        load_excerpts_df,
+    )
 
-    return get_config, load_excerpts_df, mo, pathlib, pl
+    return add_ref_poems_meta, get_config, load_excerpts_df, mo, pathlib, pl
 
 
 @app.cell
@@ -81,9 +84,53 @@ def _(excerpts_df, merged_ex_df, mo, multi_poem_id_df):
     mo.md(
         f"""
         {merged_ex_df.height:,} out of {excerpts_df.height:,} total excerpts are merges ({merged_ex_df.height/excerpts_df.height * 100:.2f}%).
-    
+
         {multi_poem_id_df.height:,} out of {merged_ex_df.height:,} merged excerpts have multiple poem ids ({multi_poem_id_df.height/merged_ex_df.height * 100:.2f}%)"""
     )
+    return
+
+
+@app.cell
+def _(add_ref_poems_meta, data_paths, multi_poem_id_df, pl):
+    # check what poem ids are getting matched / collapsed
+    poem_pairs_df = (
+        multi_poem_id_df.select(
+            "poem_id",
+            "alt_poem_ids",
+            "poem_author",
+            "poem_title",
+        )
+        .explode("alt_poem_ids")
+        .group_by("poem_id", "alt_poem_ids")
+        .agg(
+            pl.len().alias("count"),
+            pl.first("poem_author", "poem_title"),
+        )
+        .with_columns(
+            alt_poem_id=pl.col("alt_poem_ids"),  # rename to singular
+            alt_ref_corpus=pl.when(pl.col("alt_poem_ids").str.starts_with("Z"))
+            .then(pl.lit("chadwyck-healey"))
+            .otherwise(pl.lit("internet_poems")),
+        )
+        .drop("alt_poem_ids")
+    )
+    poem_pairs_df = add_ref_poems_meta(
+        poem_pairs_df,
+        data_paths["poem_meta"],
+        "alt_poem_id",
+        "alt_ref_corpus",
+        suffix="_alt",
+    )
+
+    poem_pairs_df.select(
+        "poem_id",
+        "alt_poem_id",
+        "count",
+        "poem_author",
+        "poem_title",
+        "poem_author_alt",
+        "poem_title_alt",
+    ).sort("count", descending=True)
     return
 
 
