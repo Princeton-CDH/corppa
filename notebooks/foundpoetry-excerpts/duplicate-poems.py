c5c3394d61aa7c447a7c2bc054dc3afd463c5ae1
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-04-08 13:06:12 -0400

    Expand explanatory text; check how often PL versions appear separately

--- notebooks/foundpoetry-excerpts/duplicate-poems.py
+++ notebooks/foundpoetry-excerpts/duplicate-poems.py
@@ -4,12 +4,23 @@ __generated_with = "0.19.11"
 app = marimo.App(width="medium")
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Found poem excerpts: investigate duplication
+
+    This notebook investigates duplicate poems based on excerpt data. It looks at poems that are connected based on alternate ids and exactly matching spans, and also looks at poems with a high-confidence overlapping spans.
+    """)
+    return
+
+
 @app.cell
 def _():
     import pathlib
 
     import marimo as mo
     import polars as pl
+    from polars import col as c  # for short hand column reference
 
     from corppa.config import get_config
     from corppa.poetry_detection.polars_utils import load_excerpts_df
@@ -38,7 +49,17 @@ def _():
     )
 
     excerpts_df
-    return all_excerpts_df, data_paths, excerpts_df, pl
+    return all_excerpts_df, c, data_paths, excerpts_df, mo, pl
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Exactly matching spans
+
+    Use the `alt_poem_ids` field in the excerpt data to create a dataframe of poem pairs with metadata, to see which poems have exactly matching spans, and how often that occurs.
+    """)
+    return
 
 
 @app.cell
@@ -95,11 +116,16 @@ def _(data_paths, excerpts_df, pl):
     return alt_poem_pairs_df, poem_meta_df
 
 
-app._unparsable_cell(
-    r"""
-    "from polars import col as c
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    For each unique poem id in the dataframe of poem pairs, how many poems does it match with?
+    """)
+    return
 
-    # for each unique poem id, how many poems does it match with?
+
+@app.cell
+def _(alt_poem_pairs_df, c):
     alt_poem_pairs_df.group_by("poem_id").agg(
         c.alt_poem_id.len().alias("num_poems"),
         c.poem_author.first(),
@@ -109,15 +135,20 @@ app._unparsable_cell(
         c.poem_author_alt,
         c.poem_title_alt,
         c.poem_num_lines_alt,
-    )
-    """,
-    name="_",
-)
+    ).sort("num_poems", descending=True)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Same but for alternate poem ids: for each unique alternate poem id, how many poems does it match with?
+    """)
+    return
 
 
 @app.cell
 def _(alt_poem_pairs_df, c):
-    # for each unique alternate poem id, how many poems does it match with?
     alt_poem_pairs_df.group_by("alt_poem_id").agg(
         c.poem_id.len().alias("num_poems"),
         c.poem_id,
@@ -131,8 +162,20 @@ def _(alt_poem_pairs_df, c):
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Case Study : Paradise Lost
+
+    We know we have two complete copies of Milton's _Paradise Lost_ in the Internet Poems corpus and in the Chadwyck-Healey data; there are possibly multiple partials in CH.
+
+    For the two full versions, how frequently are they recognized together? Do they match exactly, to they have a high degree of overlap?
+    """)
+    return
+
+
 @app.cell
-def _(all_excerpts_df, c):
+def _(all_excerpts_df, c, mo):
     # test with a couple of pairs
 
     poem_ids = ["John-Milton_Paradise-Lost", "Z200437755"]
@@ -148,15 +191,22 @@ def _(all_excerpts_df, c):
     ).height
     pct_with = (num_with_alt / pl_excerpts_df.height) * 100
 
-    print(
-        f"{pl_excerpts_df.height:,} excerpts with poem_id={poem_ids[0]}; {num_with_alt:,} with {poem_ids[1]} as alt id, {num_without_alt:,} without ({pct_with:.1f}% with)"
+    mo.md(
+        f"{pl_excerpts_df.height:,} excerpts with `poem_id={poem_ids[0]}; {num_with_alt:,}` with `{poem_ids[1]}` as alt id, {num_without_alt:,} without ({pct_with:.1f}% with)"
     )
     return (poem_ids,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    What are some of the examples where these ids do not occur together? (i.e.. not the exact same character span)
+    """)
+    return
+
+
 @app.cell
 def _(all_excerpts_df, c, poem_ids):
-    # what are some of the examples where the ids do not occur together?
     # go back to full excerpt data (including unset alt ids), and filter
     # to either PL poem id as primary, and alternate not set as secondary (not in list or no list)
     pl_unpairedids_df = all_excerpts_df.filter(
@@ -175,6 +225,14 @@ def _(all_excerpts_df, c):
     pl_onepage_exc_df = all_excerpts_df.filter(c.page_id.eq("CW0106390819.0044"))
     pl_onepage_exc_df
     return (pl_onepage_exc_df,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    We use the method for identifying spans with a high degree of overlap to check these, first on the context of one page with known overlap, and then on all the Paradise Lost excerpts from either copy.
+    """)
+    return
 
 
 @app.cell
@@ -211,6 +269,62 @@ def _(pl_unpaired_overlaps_df):
         "overlap_len",
         "overlap_factor",
     )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Is either one ever recognized separately? What do we lose if we simply drop one?
+    """)
+    return
+
+
+@app.cell
+def _(all_excerpts_df, c, pl, poem_ids):
+    # filter all excerpts to those for either PL id; aggregate by page
+    all_pl_excerpts_df = all_excerpts_df.filter(c.poem_id.is_in(poem_ids))
+    pl_page_excerpts_df = (
+        all_pl_excerpts_df.group_by("page_id")
+        .agg(
+            pl.len().alias("num_excerpts"),
+            c.poem_id,
+            c.alt_poem_ids.explode().unique(),
+        )
+        .with_columns(all_poem_ids=c.poem_id.list.set_union(c.alt_poem_ids))
+        .with_columns(
+            has_ch=c.all_poem_ids.list.contains(poem_ids[1]),
+            has_ip=c.all_poem_ids.list.contains(poem_ids[0]),
+        )
+        .with_columns(has_both=c.has_ch & c.has_ip)
+    )
+    pl_page_excerpts_df
+    return (pl_page_excerpts_df,)
+
+
+@app.cell
+def _(c, mo, pl_page_excerpts_df):
+    # summarize findings
+    total_pl_pages = pl_page_excerpts_df.height
+    total_pages_both = pl_page_excerpts_df.filter(c.has_both).height
+    total_ch = pl_page_excerpts_df.filter(c.has_ch).height
+    total_ip = pl_page_excerpts_df.filter(c.has_ip).height
+    mo.md(f"""
+    - {total_pl_pages:,} total pages with excerpts from _Paradise Lost_ (either of the main complete versions)
+    - {total_ch:,} pages have excerpts from Chadwyck-Healey version ({(total_ch / total_pl_pages) * 100:.0f}%)
+    - {total_ip:,} pages have excerpts from Internet Poems version ({(total_ip / total_pl_pages) * 100:.0f}%)
+    - {total_pages_both:,} pages with both ids ({(total_pages_both / total_pl_pages) * 100:.0f}%)
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Partial overlapping excerpts
+
+    Now run the method to detect substantially overlapping excerpts on all the excerpt data.
+    """)
     return
 
 
@@ -252,10 +366,16 @@ def _(all_excerpts_df, excerpt_overlaps_df, poem_meta_df):
     return (excerpt_overlap_poems_df,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Use the set of overlapping excerpts to get a list of poems with frequent high overlap.
+    """)
+    return
+
+
 @app.cell
 def _(c, excerpt_overlap_poems_df, pl):
-    # use the set of overlapping excerpts to get a list of poems with frequent high overlap
-
     # some pairs occur in either order; consolidate
     overlap_poem_pairs_df = (
         excerpt_overlap_poems_df.sort("poem_id")
@@ -282,10 +402,16 @@ def _(c, excerpt_overlap_poems_df, pl):
     return (overlap_poem_pairs_df,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Combine this information on high-overlap poem ids with the previous set of poem pairs with exactly matching spans, and save to CSV for researcher review.
+    """)
+    return
+
+
 @app.cell
 def _(alt_poem_pairs_df, overlap_poem_pairs_df):
-    # join the two sets of high-overlap poem ids
-
     poem_pairs_exact_partial_df = alt_poem_pairs_df.join(
         overlap_poem_pairs_df, on=["poem_id", "alt_poem_id"]
     ).sort("n_exact_matches", "n_90pct_overlap", descending=True)
